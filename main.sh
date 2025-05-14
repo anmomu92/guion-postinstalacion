ee08a1b81f348007469280188245118fa6525ee5
Antonio Morán Muñoz <anmomu92@gmail.com>
2025-05-14 18:23:52 +0200

    Añadido módulo requisitos y dotfile

--- main.sh
+++ main.sh
@@ -5,27 +5,28 @@
 #
 
 # ----- Constantes ----- #
-WORKING_DIR="$(pwd)"
 
 # ----- Variables ----- #
-declare -a requirements
 
 # ----- Lógica ----- #
 
+# Comprobacion de usuario
 if [ "$(id -u)" -eq 0 ]; then
     echo "##############"
     echo "Este guion no se debe ejecutar como super usuario"
-    echo "##############"
+    echo "##############"fernando diaz villanueva
 fi
 
 printf "\n:: GUION POSTINSTALACION DE ANMOMU92\n\n"
 
+# Actualizacion de repositorios
 sudo pacman --noconfirm --needed -Syu
 
-# Instalación de requisitos
-printf "\n:: SE VA A PROCEDER A LA INSTALACION DE REQUISITOS\n\n"
-mapfile -t requirements <requisitos.txt
-sudo pacman --needed -S ${requirements[@]}
+# Requisitos
+source modulos/requisitos.sh
+
+# Dotfiles
+source modulos/dotfiles.sh
 
 # Bluetooth
 source modulos/bluetooth.sh
