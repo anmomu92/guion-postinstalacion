a05050ac4c34ff2ab21a772bb539a2d35a83c575
Antonio Morán Muñoz <anmomu92@gmail.com>
2025-05-14 14:48:00 +0200

    Implementada instalación de requisitos

--- main.sh
+++ main.sh
@@ -4,6 +4,14 @@
 # Autor: anmomu92
 #
 
+# ----- Constantes ----- #
+WORKING_DIR="$(pwd)"
+
+# ----- Variables ----- #
+declare -a requirements
+
+# ----- Lógica ----- #
+
 if [ "$(id -u)" -eq 0 ]; then
     echo "##############"
     echo "Este guion no se debe ejecutar como super usuario"
@@ -13,3 +21,11 @@ fi
 printf "\n:: GUION POSTINSTALACION DE ANMOMU92\n\n"
 
 sudo pacman --noconfirm --needed -Syu
+
+# Instalación de requisitos
+printf "\n:: SE VA A PROCEDER A LA INSTALACION DE REQUISITOS\n\n"
+mapfile -t requirements <requisitos.txt
+sudo pacman --needed -S ${requirements[@]}
+
+# Bluetooth
+source modulos/bluetooth.sh
